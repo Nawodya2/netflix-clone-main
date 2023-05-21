555395bedf6e1745c721688b5b0bb8795502354f
Nawodya2 <128122077+Nawodya2@users.noreply.github.com>
2023-05-21 06:38:37 -0700

    Add files via upload

---
+++ Netflix Recreation/java.js
@@ -0,0 +1,16 @@
+///////  user scroll and change colour-navbar/////
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 100) {
+          $(".netflix-navbar").css("background" , "#0C0C0C");
+        }
+  
+        else{
+            $(".netflix-navbar").css("background" , "transparent");  	
+        }
+    });
+
+  })
+
